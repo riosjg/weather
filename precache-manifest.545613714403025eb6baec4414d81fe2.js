be7d9fa3b5fb2df971de4dd57a65d18f60954bda
Gextox <gabriel_il@live.com>
2020-07-09 21:58:00 -0300

    Updates

---
+++ precache-manifest.545613714403025eb6baec4414d81fe2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a51f4559c60ba2f6c7396e4b802c45cc",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "8d75de7ba73f1160238a",
+    "url": "/weather/static/css/main.3e82f7ce.chunk.css"
+  },
+  {
+    "revision": "7bc2162ade1c197e5072",
+    "url": "/weather/static/js/2.9f5737ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather/static/js/2.9f5737ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d75de7ba73f1160238a",
+    "url": "/weather/static/js/main.91862de8.chunk.js"
+  },
+  {
+    "revision": "65a135568c88d392bf25",
+    "url": "/weather/static/js/runtime-main.d8725fa2.js"
+  }
+]);
